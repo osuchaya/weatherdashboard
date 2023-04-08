fa25bf8fdfd1ea29df1d41a83f555e54d777bbd0
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-08 14:46:06 -0700

    Added HTML elements and basic styling

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-window.onload = function() {
-var apiKey = "ed726899eb9f958e929b2de305dd6ffb"
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat=19.4326&lon=99.1332&appid=" + apiKey)
-.then(function(response) {
-console.log("response",response)
-})
 
+window.onload = function () {
+    var apiKey = "ed726899eb9f958e929b2de305dd6ffb"
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=19.4326&lon=99.1332&appid=" + apiKey)
+        .then(function (response) {
+            console.log("response", response)
+        })
+    // tying the search form and result form to elements in HTML page
+    var searchformEl = document.querySelector('#search-form');
+    var resultContentEl = document.querySelector('#result-content');
+    var resultTextEl = document.querySelector('#result-text');
 
+    function handleSearchFormSubmit(event) {
+        event.preventDefault();
+        if (!searchInputVal) {
+            console.error('You need a search input value!');
+
+        }
+
+    }
+
+    // search query and format values:
 
 }
