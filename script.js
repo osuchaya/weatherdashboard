c926f534d5b1ced0a97b7510a34c692130da5e1d
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-09 16:49:44 -0700

    Add weather icons and current day weather code

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ window.onload = function () {
     // tying the search form and result form to elements in HTML page
     var searchInputEl = document.querySelector('#search-input');
     var resultContentEl = document.querySelector('#result-content');
-    var resultTextEl = document.querySelector('#result-text');
+    var dashboardEl = document.querySelector('#dashboard');
     var searchFormEl = document.querySelector('#search-form');
+dashboardEl.style.display = "none"
 
     function handleSearchFormSubmit(event) {
         event.preventDefault();
      
         var cityName = searchInputEl.value.trim();
         if (cityName) {
+            dashboardEl.style.display = "block";
+            getCurrentWeather(cityName);
             getWeatherForecast(cityName);
             addCitytoHistory(cityName);
             updateHistoryHTML();
@@ -30,14 +33,19 @@ window.onload = function () {
 
 function addCitytoHistory(cityName) {
     var history = JSON.parse(localStorage.getItem("history"))
-    history.push(cityName)
+    if (history) {
+        history.push(cityName)
+    } else {
+        history = [cityName]
+    }
+    
     localStorage.setItem("history",JSON.stringify(history))
 
 }
 function updateHistoryHTML() {
     var history = JSON.parse(localStorage.getItem("history"))
     var historyElement = document.querySelector("#history")
-
+    historyElement.innerHTML = '';
     for (i=0; i<history.length; i++) {
         var cityName = history[i]
         var cardEl = document.createElement('div');
@@ -46,12 +54,43 @@ function updateHistoryHTML() {
     }
 
 }
+
+function getCurrentWeather(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+
+               
+                    var day = data
+                    getDayWeather(day)
+                    var [date, temp, wind, humidity, image] = getDayWeather(day)
+                    var forecastCardEl = document.querySelector("#today")
+                    var dateEl = forecastCardEl.querySelector('.date')
+                    var tempEl = forecastCardEl.querySelector('.temp')
+                    var windEl = forecastCardEl.querySelector('.wind')
+                    var humidityEl = forecastCardEl.querySelector('.humidity')
+                    var imageEl = forecastCardEl.querySelector('.icon img')
+                    dateEl.textContent = cityName + " " + date
+                    tempEl.textContent = temp.toFixed(2) + " F"
+                    windEl.textContent = wind + " MPH"
+                    humidityEl.textContent = humidity + "%"
+                    imageEl.setAttribute('src', image)
+                
+
+            })
+        }
+    })
+}
     function getDayWeather(day) {
-        var dayDate = day.dt_txt.substring(0, 11)
+        var dayDate = dayjs.unix(day.dt).format('MM/DD/YYYY')
         var dayTemp = (day.main.temp - 273.15) * (9 / 5) + 32
         var dayWind = day.wind.speed
         var dayHumidity = day.main.humidity
-        return [dayDate, dayTemp, dayWind, dayHumidity]
+        var dayImage = "https://openweathermap.org/img/wn/"+ day.weather[0].icon.replace("n","d") + "@2x.png"
+        return [dayDate, dayTemp, dayWind, dayHumidity, dayImage]
     }
     var getWeatherForecast = function (cityName) {
         var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey
@@ -64,17 +103,18 @@ function updateHistoryHTML() {
                     for (i = 0; i < 5; i++) {
                         var day = data.list[i * 8 + 4]
                         getDayWeather(day)
-                        var [date, temp, wind, humidity] = getDayWeather(day)
+                        var [date, temp, wind, humidity, image] = getDayWeather(day)
                         var forecastCardEl = document.querySelector("#day" + (i + 1))
                         var dateEl = forecastCardEl.querySelector('.date')
                         var tempEl = forecastCardEl.querySelector('.temp')
                         var windEl = forecastCardEl.querySelector('.wind')
                         var humidityEl = forecastCardEl.querySelector('.humidity')
+                        var imageEl = forecastCardEl.querySelector('.icon img')
                         dateEl.textContent = date
                         tempEl.textContent = temp.toFixed(2) + " F"
                         windEl.textContent = wind + " MPH"
                         humidityEl.textContent = humidity + "%"
-
+                        imageEl.setAttribute('src', image)
                     }
 
                 })
